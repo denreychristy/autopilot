f1581c67915de93b451472391b7ad44fd772cb47
Denrey Christy <denreyc@gmail.com>
2025-11-24 15:55:37 -0500

    2025-11-24-03-55

--- main.py
+++ main.py
@@ -3,38 +3,40 @@
 # ================================================================================================ #
 # Imports
 
+import sys
+import pygame as pg
+
 from rust import Currency # type: ignore
 from rust import NonPlayerCharacter as NPC # type: ignore
+
+from settings import Settings
+from debug import debug
 
 # ================================================================================================ #
 
 class AutoPilot:
 	def __init__(self):
-		currency = Currency(
-			"dollar",
-			"dollars",
-			0
-		)
+		self.settings = Settings()
 
-		npc = NPC()
-		print(npc.get_id(), npc.get_currency())
+		pg.init()
+		self.screen = pg.display.set_mode((self.settings.WIDTH, self.settings.HEIGHT))
+		pg.display.set_caption('Autopilot')
+		self.clock = pg.time.Clock()
 	
-	# ================================================== #
-	# Class Methods
-
-	# ================================================== #
-	# Dunder Methods
-
-	# ================================================== #
-	# Property Methods
-
-	# ================================================== #
-	# Set Methods
-
-	# ================================================== #
-	# Other Methods
+	def run(self):
+		while True:
+			for event in pg.event.get():
+				if event.type == pg.QUIT:
+					pg.quit()
+					sys.exit()
+			
+			self.screen.fill('black')
+			debug('Debug')
+			pg.display.update()
+			self.clock.tick(self.settings.FPS)
 
 # ================================================================================================ #
 
 if __name__ == '__main__':
-	AutoPilot()
+	autopilot = AutoPilot()
+	autopilot.run()
